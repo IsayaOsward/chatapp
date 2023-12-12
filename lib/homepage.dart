df7a15db423c3a00ab31f61094dd0840e2e43f29
isaya <ionthefirst97@gmail.com>
2023-12-12 16:28:05 +0300

    Reamining drawer and fuctionaliyu

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/useravatar.dart';
 
@@ -145,6 +147,7 @@ class Home extends StatelessWidget {
             right: 0,
             bottom: 0,
             child: Container(
+              padding: EdgeInsets.symmetric(vertical: 15),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -152,74 +155,107 @@ class Home extends StatelessWidget {
                     topRight: Radius.circular(40),
                   )),
               child: ListView(
+                padding: EdgeInsets.only(left: 25),
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Row(
-                              children: [
-                                UserAvatar(filename: "alice"),
-                                SizedBox(width: 5),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Alice",
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(
-                                      height: 5,
-                                    ),
-                                    Text(
-                                      "Hello John, how are you doing?",
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 18,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 15),
-                            child: Column(
-                              children: [
-                                Text("16:51"),
-                                SizedBox(
-                                  height: 15,
-                                ),
-                                CircleAvatar(
-                                  backgroundColor: Color(0xFF27c1a9),
-                                  radius: 7,
-                                  child: Text(
-                                    "2",
-                                    style: TextStyle(
-                                        fontSize: 10, color: Colors.white),
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
+                  BuildConversation(
+                      "Jesca", "I have sent you these pictures", "Jesca", 10),
+                  BuildConversation(
+                      "Bless", "Is your girlfriend at home?", "bless", 0),
+                  BuildConversation("Cathy",
+                      "I need to go pick my brother at school", "cathy", 1),
+                  BuildConversation(
+                      "bob",
+                      "I have becaome more superior in CTF challenges",
+                      "bob",
+                      2),
+                  BuildConversation(
+                      "Oliver", "Thank you so much peter", "oliver", 0),
+                  BuildConversation("Glory",
+                      "Hi there! i'm trying to do code everyday", "glory", 0),
+                  BuildConversation("Happy",
+                      "Thanks for coming, im glad i saw you", "happy", 0),
+                  BuildConversation(
+                      "james", "Hey, peter did you check George", "james", 3),
+                  BuildConversation(
+                      "George", "Your project is very impressive", "George", 2),
+                  BuildConversation("Kayla",
+                      "I'm so excited today please come visit me", "kayla", 1),
                 ],
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Column BuildConversation(
+      String name, String message, String filename, int msgCount) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                UserAvatar(filename: filename),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      message,
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 25),
+              child: Column(
+                children: [
+                  Text(
+                    "16:51",
+                    style: TextStyle(),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  if (msgCount > 0)
+                    CircleAvatar(
+                      radius: 7,
+                      backgroundColor: Color(0xFF27c1a9),
+                      child: Text(
+                        msgCount.toString(),
+                        style: TextStyle(color: Colors.white, fontSize: 10),
+                      ),
+                    ),
+                ],
+              ),
+            )
+          ],
+        ),
+        Divider(
+          height: 5,
+          thickness: 0.5,
+          indent: 70,
+          color: Colors.grey.shade300,
+        )
+      ],
     );
   }
 
